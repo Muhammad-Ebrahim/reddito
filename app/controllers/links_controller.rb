09050b1700e69bcf96e6e5e6dda7716711d8514f
Muhammad Ibrahim <muhammed.ibrahim011149@gmail.com>
2017-11-29 13:54:49 +0200

    final commit

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -66,13 +66,11 @@ class LinksController < ApplicationController
   def upvote
     @link = Link.find(params[:id])
     @link.upvote_by current_user
-    redirect_to :back
   end
 
   def downvote
     @link = Link.find(params[:id])
     @link.downvote_from current_user
-    redirect_to :back
   end
 
   private
